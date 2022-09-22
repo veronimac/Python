20450c0c997b73cbfa2f597c6d40418ff716af20
unknown <veroni_krolik@mail.ru>
2022-09-22 12:03:28 +0300

    added dict tasks

--- dictTASKS/Task2.py
+++ dictTASKS/Task2.py
@@ -3,3 +3,7 @@
 а в качестве значения количество этих цифр в строке
 """
 numbers = "0139412831055230677798"
+numbers_dict={}
+for i in range(10):
+    numbers_dict[i]=numbers.count(str(i))
+print(numbers_dict)
